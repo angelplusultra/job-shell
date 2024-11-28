f68ce18fe73014b4ee80def628f8421d5bb5e6a7
Marcello Fitton <macfittondev@gmail.com>
2024-11-27 19:07:47 -0800

    Implemented Blizzard

--- src/scrapers/blizzard/scraper.rs
+++ src/scrapers/blizzard/scraper.rs
@@ -9,7 +9,7 @@ use crate::models::{
 
 pub async fn scrape_blizzard(data: &mut Data) -> Result<JobsPayload, Box<dyn Error>> {
     let launch_options = LaunchOptions {
-        headless: false,
+        headless: true,
         window_size: Some((1920, 1080)),
         enable_logging: true,
 
@@ -19,52 +19,62 @@ pub async fn scrape_blizzard(data: &mut Data) -> Result<JobsPayload, Box<dyn Err
 
     let tab = browser.new_tab()?;
 
-    tab.navigate_to("https://careers.blizzard.com/global/en/search-results?rk=l-engineering-technology&sortBy=Most%20relevant")?;
+    tab.navigate_to("https://careers.blizzard.com/global/en/search-results?from=0&s=1&rk=l-engineering-technology")?;
     tab.wait_for_element("body")?;
     tab.wait_for_element(".results-state.container")?;
 
-    //     let scraped_jobs = tab.evaluate(
-    //         r##"
-    //
-    // // DELETE AND REPLACE WITH CUSTOM JS LOGIC
-    // const engJobs = document.querySelector("#jobs-16253")
-    //
-    // const jobsPayload = Array.from(engJobs.querySelectorAll(".job")).map(j => {
-    //     const title = j.querySelector(".job-title").innerHTML;
-    //     const location = j.querySelector(".job-location").innerHTML;
-    //     const link = j.querySelector("a").href;
-    //
-    //     return {
-    //         title,
-    //         location,
-    //         link
-    //     }
-    // })
-    //
-    // JSON.stringify(jobsPayload);
-    //     "##,
-    //         false,
-    //     )?;
+    let js = r#"
+    const jobsList = document.querySelector('ul[data-ph-id="ph-page-element-page14-X5QcnJ"]');
 
-    let mut button_result = tab.wait_for_element("#acc-skip-content > div.body-wrapper.ph-page-container > div > div > div > div.col-lg-8.col-md-8.col-sm-7 > section:nth-child(2) > div > div > div > div.pagination-block.au-target > ul > li:nth-child(5) > a");
+const jobs = [...jobsList.querySelectorAll("li.jobs-list-item")].map(jobNode => {
 
-    while let Ok(button) = button_result {
-        button.click()?;
-        tab.evaluate("window.scrollTo(0, document.body.scrollHeight);", false)?;
-        tab.wait_for_element("#jobs-list-item")?;
-        button_result = tab.find_element("#acc-skip-content > div.body-wrapper.ph-page-container > div > div > div > div.col-lg-8.col-md-8.col-sm-7 > section:nth-child(2) > div > div > div > div.pagination-block.au-target > ul > li:nth-child(5) > a");
+    const title = jobNode.querySelector("span").innerText;
+    const link = jobNode.querySelector("a").href;
+    const locationNode = jobNode.querySelector("span.job-location");
+    let location;
+        
+  if(locationNode) {
+      location = locationNode.innerText.split("\n")[1].trim();
+      
+  } else {
+      const locations = [...jobNode.querySelectorAll("li.au-target.location")];
+
+      location = locations.map(loc => loc.innerText.trim()).join(" | ");
+  }   
+        
+        
+    return {
+        title,
+        link,
+        location
+    }
+})
+
+JSON.stringify(jobs)
+
+    "#;
+
+    let mut results = 0;
+    let mut scraped_jobs: Vec<ScrapedJob> = Vec::new();
+    loop {
+        tab.navigate_to(&format!("https://careers.blizzard.com/global/en/search-results?from={}&s=1&rk=l-engineering-technology", results))?;
+        tab.wait_for_element("body")?;
+        tab.wait_for_element(".results-state.container")?;
+        let remote_object = tab.evaluate(js, false)?;
+
+        if let Some(value) = remote_object.value {
+            let scraped_jobs_subset: Vec<ScrapedJob> = serde_json::from_str(value.as_str().unwrap())?;
+            scraped_jobs.extend(scraped_jobs_subset);
+        } else {
+            break;
+        }
+
+        results += 10;
     }
 
-    todo!();
-    // let scraped_jobs: Vec<ScrapedJob> =
-    //     serde_json::from_str(scraped_jobs.value.unwrap().as_str().unwrap()).unwrap();
-    //
-    // let jobs_payload = JobsPayload::from_scraped_jobs(scraped_jobs, &data.data["Blizzard"]);
-    //
-    // data.data.get_mut("Blizzard").unwrap().jobs = jobs_payload.all_jobs.clone();
-    //
-    // data.save();
+    let jobs_payload = JobsPayload::from_scraped_jobs(scraped_jobs, &data.data["Blizzard"]);
+    data.data.get_mut("Blizzard").unwrap().jobs = jobs_payload.all_jobs.clone();
+    data.save();
 
-    // Ok(jobs_payload)
+    Ok(jobs_payload)
 }
-
