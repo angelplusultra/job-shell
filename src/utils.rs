07955365898b36c50d7f0766a293ff0aae62b387
angelplusultra <macfittondev@gmail.com>
2024-12-12 04:24:23 -0800

    Created stall helper fns

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,21 @@
-use std::io::Write;
+use std::{ thread::sleep, time::Duration};
+use std::io::{self, Write};
 pub fn clear_console() {
     print!("\x1B[2J\x1B[1;1H");
     std::io::stdout().flush().unwrap()
+}
+
+pub fn stall_program(seconds: u64) {
+    sleep(Duration::from_secs(seconds));
+}
+
+pub fn stall_and_present_countdown(start: u32, message: Option<&'static str>) {
+
+    for i in (1..=start).rev() {
+        
+        print!("\r{}: {}",message.unwrap_or_default(), i); // Print the number on the same line
+        io::stdout().flush().unwrap(); // Flush to ensure immediate output
+        sleep(Duration::from_secs(1)); // Pause for the specified duration
+    }
+    println!("\rGo!     "); // Clear the line and print "Go!"
 }
